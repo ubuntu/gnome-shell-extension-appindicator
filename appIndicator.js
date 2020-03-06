ubuntu/gnome-shell-extension-appindicator

commit 2af8284493c4631b5584da4a976d1e8534cba9b8

util: Use critical instead of fatal logging level

appIndicator.js
@@ -346,7 +346,7 @@ class AppIndicators_IconActor extends Shell.Stack {
             //HACK: icon is a path name. This is not specified by the api but at least inidcator-sensors uses it.
             var [ format, width, height ] = GdkPixbuf.Pixbuf.get_file_info(icon_name)
             if (!format) {
-                Util.Logger.fatal("invalid image format: "+icon_name)
+                Util.Logger.critical("invalid image format: "+icon_name)
             } else {
                 // if the actual icon size is smaller, save that for later.
                 // scaled icons look ugly.
@@ -498,7 +498,7 @@ class AppIndicators_IconActor extends Shell.Stack {
         }
 
         if (!newIcon) {
-            Util.Logger.fatal("unable to update icon");
+            Util.Logger.critical("unable to update icon");
             return;
         }
 

dbusMenu.js
@@ -145,7 +145,7 @@ var DbusMenuItem = class AppIndicators_DbusMenuItem {
         }
 
         if (pos < 0) {
-            Util.Logger.fatal("Trying to remove child which doesn't exist")
+            Util.Logger.critical("Trying to remove child which doesn't exist")
         } else {
             this._children_ids.splice(pos, 1)
             this.emit('child-removed', this._client.get_item(child_id))
@@ -163,7 +163,7 @@ var DbusMenuItem = class AppIndicators_DbusMenuItem {
         }
 
         if (oldpos < 0) {
-            Util.Logger.fatal("tried to move child which wasn't in the list")
+            Util.Logger.critical("tried to move child which wasn't in the list")
             return
         }
 

util.js
@@ -233,7 +233,7 @@ var Logger = class AppIndicators_Logger {
         Logger._logStructured(GLib.LogLevelFlags.LEVEL_ERROR, message);
     }
 
-    static fatal(message) {
+    static critical(message) {
         Logger._logStructured(GLib.LogLevelFlags.LEVEL_CRITICAL, message);
     }
 };

commit 96a32c1bcc6c173c8850a3b57b2c3504e37ae1bf

Use correct logging method
Fixes an issue introduced with 2af8284493c4631b5584da4a976d1e8534cba9b8
where suddenly errors like these appeard in my systemd log:

```
gnome-shell[5070]: JS ERROR: Exception in callback for signal: icon: TypeError: Util.Logger.fatal is not a function
_getIconInfo@/home/takuto/.local/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com/appIndicator.js:408:21
_cacheOrCreateIconByName@/home/takuto/.local/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com/appIndicator.js:334:27
_updateIcon@/home/takuto/.local/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com/appIndicator.js:513:27
_emit@resource:///org/gnome/gjs/modules/signals.js:135:27
_onPropertiesChanged/<@/home/takuto/.local/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com/appIndicator.js:214:17
_onPropertiesChanged@/home/takuto/.local/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com/appIndicator.js:208:9
refreshPropertyOnProxy/<@/home/takuto/.local/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com/util.js:42:37
```

I also changed the log level to debug because this is not critical and only noisy
logging IMHO. Critical / fatal errors should lead to program
termination which is not the case here.

appIndicator.js
@@ -405,7 +405,7 @@ class AppIndicators_IconActor extends Shell.Stack {
                                                   Gtk.IconLookupFlags.GENERIC_FALLBACK);
                 // no icon? that's bad!
                 if (iconInfo === null) {
-                    Util.Logger.fatal("unable to lookup icon for " + name);
+                    Util.Logger.debug("unable to lookup icon for " + name);
                 } else { // we have an icon
                     // the icon size may not match the requested size, especially with custom themes
                     if (iconInfo.get_base_size() < size) {