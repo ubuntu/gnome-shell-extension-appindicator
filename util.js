c1453fa8a27cf270ef3833d4b9e231268634f94f
Dennis Fölster <3sr3vn1@gmail.com>
2020-12-28 11:55:31 +0100

    Fix incorrect placed import.

--- util.js
+++ util.js
@@ -16,6 +16,7 @@
 const Gio = imports.gi.Gio
 const GLib = imports.gi.GLib
 const Gtk = imports.gi.Gtk;
+const Gdk = imports.gi.Gdk;
 const Main = imports.ui.main;
 const GObject = imports.gi.GObject
 const Extension = imports.misc.extensionUtils.getCurrentExtension();
