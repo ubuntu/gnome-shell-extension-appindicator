ubuntu/gnome-shell-extension-appindicator

commit 3b02297b8fdc7840479baa6849474be9f65b506a

cleanup: Replace more GLib Sources with promises

iconCache.js
@@ -17,7 +17,9 @@
 const GLib = imports.gi.GLib
 const Gio = imports.gi.Gio
 
-const Util = imports.misc.extensionUtils.getCurrentExtension().imports.util;
+const Extension = imports.misc.extensionUtils.getCurrentExtension();
+const PromiseUtils = Extension.imports.promiseUtils;
+const Util = Extension.imports.util;
 
 // The icon cache caches icon objects in case they're reused shortly aftwerwards.
 // This is necessary for some indicators like skype which rapidly switch between serveral icons.
@@ -103,18 +105,17 @@ var IconCache = class AppIndicators_IconCache {
         return null;
     }
 
-    _checkGC() {
+    async _checkGC() {
         let cacheIsEmpty = this._cache.size == 0;
 
         if (!cacheIsEmpty && !this._gcTimeout) {
             Util.Logger.debug("IconCache: garbage collector started");
-            this._gcTimeout = GLib.timeout_add_seconds(
-                GLib.PRIORITY_LOW,
-                GC_INTERVAL,
-                () => this._gc());
+            this._gcTimeout = new PromiseUtils.TimeoutSecondsPromise(GC_INTERVAL,
+                GLib.PRIORITY_LOW);
+            await this._gcTimeout;
         } else if (cacheIsEmpty && this._gcTimeout) {
             Util.Logger.debug("IconCache: garbage collector stopped");
-            GLib.Source.remove(this._gcTimeout);
+            this._gcTimeout.cancel();
             delete this._gcTimeout;
         }
     }

appIndicator.js
@@ -85,15 +85,7 @@ var AppIndicator = class AppIndicators_AppIndicator {
                 try {
                     initable.init_finish(result);
                     this._checkIfReady();
-
-                    if (!this.isReady && !this.menuPath) {
-                        let checks = 0;
-                        this._delayCheck = GLib.timeout_add_seconds(
-                            GLib.PRIORITY_DEFAULT_IDLE, 1, () => {
-                            Util.refreshPropertyOnProxy(this._proxy, 'Menu');
-                            return !this.isReady && ++checks < 3;
-                        });
-                    }
+                    this._checkMenuReady();
                 } catch(e) {
                     if (!e.matches(Gio.IOErrorEnum, Gio.IOErrorEnum.CANCELLED))
                         Util.Logger.warn(`While intializing proxy for ${bus_name} ${object}: ${e}`);
@@ -117,7 +109,7 @@ var AppIndicator = class AppIndicators_AppIndicator {
 
         if (this.isReady && !wasReady) {
             if (this._delayCheck) {
-                GLib.Source.remove(this._delayCheck);
+                this._delayCheck.cancel();
                 delete this._delayCheck;
             }
 
@@ -128,9 +120,26 @@ var AppIndicator = class AppIndicators_AppIndicator {
         return false;
     }
 
+    async _checkMenuReady() {
+        if (this.menuPath)
+            return true;
+
+        const cancellable = this._cancellable;
+        for (let checks = 0; checks < 3 && !this.isReady; ++checks) {
+            this._delayCheck = new PromiseUtils.TimeoutSecondsPromise(1,
+                GLib.PRIORITY_DEFAULT_IDLE, cancellable);
+            await this._delayCheck;
+            Util.refreshPropertyOnProxy(this._proxy, 'Menu');
+        }
+
+        return !!this.menuPath;
+    }
+
     _nameOwnerChanged() {
         if (!this._proxy.g_name_owner)
             this._checkIfReady();
+        else
+            this._checkMenuReady();
     }
 
     _addExtraProperty(name) {
@@ -184,18 +193,21 @@ var AppIndicator = class AppIndicators_AppIndicator {
             );
     }
 
-    _onProxySignal(_proxy, _sender, signal, _params) {
+    async _onProxySignal(_proxy, _sender, signal, _params) {
         this._accumuledSignals.add(signal);
 
-        if (this._signalsAccumulatorId)
+        if (this._signalsAccumulator)
             return;
 
-        this._signalsAccumulatorId = GLib.timeout_add(
-            GLib.PRIORITY_DEFAULT_IDLE, MAX_UPDATE_FREQUENCY, () => {
-                this._accumuledSignals.forEach((s) => this._translateNewSignals(s));
-                this._accumuledSignals.clear();
-                delete this._signalsAccumulatorId;
-            });
+        this._signalsAccumulator = new PromiseUtils.TimeoutPromise(
+            GLib.PRIORITY_DEFAULT_IDLE, MAX_UPDATE_FREQUENCY, this._cancellable);
+        try {
+            await this._signalsAccumulator;
+            this._accumuledSignals.forEach((s) => this._translateNewSignals(s));
+            this._accumuledSignals.clear();
+        } finally {
+            delete this._signalsAccumulator;
+        }
     }
 
     //public property getters
@@ -298,17 +310,7 @@ var AppIndicator = class AppIndicators_AppIndicator {
         this._cancellable.cancel();
         Util.cancelRefreshPropertyOnProxy(this._proxy);
         delete this._cancellable;
-        delete this._proxy
-
-        if (this._signalsAccumulatorId) {
-            GLib.Source.remove(this._signalsAccumulatorId);
-            delete this._signalsAccumulatorId;
-        }
-
-        if (this._delayCheck) {
-            GLib.Source.remove(this._delayCheck);
-            delete this._delayCheck;
-        }
+        delete this._proxy;
     }
 
     open() {

dbusMenu.js
@@ -25,6 +25,7 @@ const St = imports.gi.St
 const Extension = imports.misc.extensionUtils.getCurrentExtension()
 
 const DBusInterfaces = Extension.imports.interfaces
+const PromiseUtils = Extension.imports.promiseUtils;
 const Util = Extension.imports.util
 
 //////////////////////////////////////////////////////////////////////////
@@ -249,14 +250,16 @@ var DBusClient = class AppIndicators_DBusClient {
             this._beginLayoutUpdate()
     }
 
-    _requestProperties(id) {
-        // if we don't have any requests queued, we'll need to add one
-        if (!this._propertiesRequestId) {
-            this._propertiesRequestId = GLib.idle_add(
-                GLib.PRIORITY_DEFAULT_IDLE, () => this._beginRequestProperties())
-        }
-
+    async _requestProperties(id) {
         this._propertiesRequestedFor.add(id);
+
+        // if we don't have any requests queued, we'll need to add one
+        if (!this._propertiesRequest || !this._propertiesRequest.pending()) {
+            this._propertiesRequest = new PromiseUtils.IdlePromise(
+                GLib.PRIORITY_DEFAULT_IDLE, this._cancellable);
+            await this._propertiesRequest;
+            this._beginRequestProperties();
+        }
     }
 
     _beginRequestProperties() {
@@ -267,8 +270,6 @@ var DBusClient = class AppIndicators_DBusClient {
                 this._endRequestProperties.bind(this))
 
         this._propertiesRequestedFor.clear();
-        delete this._propertiesRequestId;
-
         return false
     }
 
@@ -463,11 +464,6 @@ var DBusClient = class AppIndicators_DBusClient {
 
     destroy() {
         this.emit('destroy')
-
-        if (this._propertiesRequestId) {
-            GLib.Source.remove(this._propertiesRequestId);
-            delete this._propertiesRequestId;
-        }
 
         this._cancellable.cancel();
         Signals._disconnectAll.apply(this._proxy)

util.js
@@ -21,6 +21,7 @@ const Main = imports.ui.main;
 const GObject = imports.gi.GObject
 const Extension = imports.misc.extensionUtils.getCurrentExtension();
 const Params = imports.misc.params;
+const PromiseUtils = Extension.imports.promiseUtils;
 
 var refreshPropertyOnProxy = function(proxy, propertyName, params) {
     if (!proxy._proxyCancellables)
@@ -61,16 +62,12 @@ var refreshPropertyOnProxy = function(proxy, propertyName, params) {
                 proxy._proxyChangedProperties = {};
             proxy._proxyChangedProperties[propertyName] = valueVariant;
 
-            if (!proxy._proxyPropertiesEmitId) {
-                proxy._proxyPropertiesEmitId = GLib.timeout_add(
-                    GLib.PRIORITY_DEFAULT_IDLE, 16, () => {
-                    delete proxy._proxyPropertiesEmitId;
-
+            if (!proxy._proxyPropertiesEmit || !proxy._proxyPropertiesEmit.pending()) {
+                proxy._proxyPropertiesEmit = new PromiseUtils.TimeoutPromise(16,
+                    GLib.PRIORITY_DEFAULT_IDLE, cancellable).then(() => {
                     proxy.emit('g-properties-changed', GLib.Variant.new('a{sv}',
                         proxy._proxyChangedProperties), []);
                     delete proxy._proxyChangedProperties;
-
-                    return GLib.SOURCE_REMOVE;
                 });
             }
         } catch (e) {
@@ -108,10 +105,6 @@ var cancelRefreshPropertyOnProxy = function(proxy, params) {
             return cancellable;
         }
     } else {
-        if (proxy._proxyPropertiesEmitId) {
-            GLib.source_remove(proxy._proxyPropertiesEmitId);
-            delete proxy._proxyPropertiesEmitId;
-        }
         proxy._proxyCancellables.forEach(c => c.cancel());
         delete proxy._proxyChangedProperties;
         delete proxy._proxyCancellables;

commit cdbf38f8bd99b4ce4a69d5e6de4ff4558eed9b7e

iconCache: Actually perform Garbage Collection
Fix an issue introduced with commit 3b02297b8f, causing the iconCache not
to be ever garbage collected.

iconCache.js
@@ -115,6 +115,7 @@ var IconCache = class AppIndicatorsIconCache {
                 GLib.PRIORITY_LOW);
             try {
                 await this._gcTimeout;
+                this._gc();
             } catch (e) {
                 if (!e.matches(Gio.IOErrorEnum, Gio.IOErrorEnum.CANCELLED))
                     logError(e, 'IconCache: garbage collector');