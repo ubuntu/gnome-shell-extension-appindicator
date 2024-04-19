99e1911bb64f4664aad56ce3f3f47a82fe5b7c0d
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2024-04-19 10:44:05 +0200

    Little shufle

--- dbusMenu.js
+++ dbusMenu.js
@@ -517,12 +517,12 @@ export const DBusClient = GObject.registerClass({
                 ret.is_of_type(new GLib.VariantType('()')))
                 this._requestLayoutUpdate();
         } catch (e) {
+            Util.Logger.debug(`Error when calling 'AboutToShow()' in ${this.gName}, ${this.gObjectPath}, ${this.gInterfaceName}`);
             if (e.matches(Gio.DBusError, Gio.DBusError.UNKNOWN_METHOD) ||
                 e.matches(Gio.DBusError, Gio.DBusError.FAILED)) {
                 this._hasAboutToShow = false;
                 return;
             }
-            Util.Logger.debug(`Error when calling 'AboutToShow()' in ${this.gName}, ${this.gObjectPath}, ${this.gInterfaceName}`);
             if (!e.matches(Gio.IOErrorEnum, Gio.IOErrorEnum.CANCELLED))
                 logError(e);
         }
